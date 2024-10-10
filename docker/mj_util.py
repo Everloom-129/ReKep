89cb13090248d428ae3cfd7a2d84c46d9d7e9551
everloom <Everloom-129>
2024-10-09 23:33:50 -0400

    WIP MJX test

---
+++ docker/mj_util.py
@@ -0,0 +1,50 @@
+#@title Import packages for plotting and creating graphics
+import time
+import itertools
+import numpy as np
+from typing import Callable, NamedTuple, Optional, Union, List
+
+# Graphics and plotting.
+# print('Installing mediapy:')
+# !command -v ffmpeg >/dev/null || (apt update && apt install -y ffmpeg)
+# !pip install -q mediapy
+import mediapy as media
+import matplotlib.pyplot as plt
+
+# More legible printing from numpy.
+np.set_printoptions(precision=3, suppress=True, linewidth=100)
+
+
+
+#@title Import MuJoCo, MJX, and Brax
+from datetime import datetime
+from etils import epath
+import functools
+from IPython.display import HTML
+from typing import Any, Dict, Sequence, Tuple, Union
+import os
+from ml_collections import config_dict
+
+
+import jax
+from jax import numpy as jp
+import numpy as np
+from flax.training import orbax_utils
+from flax import struct
+from matplotlib import pyplot as plt
+import mediapy as media
+from orbax import checkpoint as ocp
+
+import mujoco
+from mujoco import mjx
+
+from brax import base
+from brax import envs
+from brax import math
+from brax.base import Base, Motion, Transform
+from brax.base import State as PipelineState
+from brax.envs.base import Env, PipelineEnv, State
+from brax.mjx.base import State as MjxState
+from brax.training.agents.ppo import train as ppo
+from brax.training.agents.ppo import networks as ppo_networks
+from brax.io import html, mjcf, model
